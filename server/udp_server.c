02252cb8c61ad3d7364c122952d7cac02d8e12e6
Jacob Malcy <pcx436@yahoo.com>
2020-09-20 01:04:58 -0600

    server changes
    Is it bad practice to commit an entire file at once? yes!

--- server/udp_server.c
+++ server/udp_server.c
@@ -43,92 +43,150 @@ void ls(char results[]) {
 }
 
 int main(int argc, char **argv) {
-  int sockfd; /* socket */
-  int portno; /* port to listen on */
-  int clientlen; /* byte size of client's address */
-  struct sockaddr_in serveraddr; /* server's addr */
-  struct sockaddr_in clientaddr; /* client addr */
-  struct hostent *hostp; /* client host info */
-  char buf[BUFSIZE]; /* message buf */
-  char *hostaddrp; /* dotted decimal host addr string */
-  int optval; /* flag value for setsockopt */
-  int n; /* message byte size */
+	int sockfd; /* socket */
+	int portno; /* port to listen on */
+	int clientlen; /* byte size of client's address */
+	struct sockaddr_in serveraddr; /* server's addr */
+	struct sockaddr_in clientaddr; /* client addr */
+	struct hostent *hostp; /* client host info */
+	char buf[BUFSIZE]; /* message buf */
+	char *hostaddrp, *command, *parameter, *end; /* dotted decimal host addr string */
+	int optval; /* flag value for setsockopt */
+	int n, keepRunning = 1; /* message byte size */
 
-  /* 
-   * check command line arguments 
-   */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
-  portno = atoi(argv[1]);
+	/*
+	 * check command line arguments
+	 */
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+	portno = atoi(argv[1]);
 
-  /* 
-   * socket: create the parent socket 
-   */
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sockfd < 0) 
-    error("ERROR opening socket");
+	/*
+	 * socket: create the parent socket
+	 */
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
 
-  /* setsockopt: Handy debugging trick that lets 
-   * us rerun the server immediately after we kill it; 
-   * otherwise we have to wait about 20 secs. 
-   * Eliminates "ERROR on binding: Address already in use" error. 
-   */
-  optval = 1;
-  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
-	     (const void *)&optval , sizeof(int));
+	/* setsockopt: Handy debugging trick that lets
+	 * us rerun the server immediately after we kill it;
+	 * otherwise we have to wait about 20 secs.
+	 * Eliminates "ERROR on binding: Address already in use" error.
+	 */
+	optval = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+	           (const void *) &optval, sizeof(int));
 
-  /*
-   * build the server's Internet address
-   */
-  bzero((char *) &serveraddr, sizeof(serveraddr));
-  serveraddr.sin_family = AF_INET;
-  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serveraddr.sin_port = htons((unsigned short)portno);
+	/*
+	 * build the server's Internet address
+	 */
+	bzero((char *) &serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short) portno);
 
-  /* 
-   * bind: associate the parent socket with a port 
-   */
-  if (bind(sockfd, (struct sockaddr *) &serveraddr, 
-	   sizeof(serveraddr)) < 0) 
-    error("ERROR on binding");
+	/*
+	 * bind: associate the parent socket with a port
+	 */
+	if (bind(sockfd, (struct sockaddr *) &serveraddr,
+	         sizeof(serveraddr)) < 0)
+		error("ERROR on binding");
 
-  /* 
-   * main loop: wait for a datagram, then echo it
-   */
-  clientlen = sizeof(clientaddr);
-  while (1) {
+	/*
+	 * main loop: wait for a datagram, then echo it
+	 */
+	clientlen = sizeof(clientaddr);
+	while (keepRunning == 1) {
 
-    /*
-     * recvfrom: receive a UDP datagram from a client
-     */
-    bzero(buf, BUFSIZE);
-    n = recvfrom(sockfd, buf, BUFSIZE, 0,
-		 (struct sockaddr *) &clientaddr, &clientlen);
-    if (n < 0)
-      error("ERROR in recvfrom");
+		/*
+		 * recvfrom: receive a UDP datagram from a client
+		 */
+		bzero(buf, BUFSIZE);
+		n = recvfrom(sockfd, buf, BUFSIZE, 0,
+		             (struct sockaddr *) &clientaddr, &clientlen);
+		if (n < 0)
+			error("ERROR in recvfrom");
+		/*
+		 * gethostbyaddr: determine who sent the datagram
+		 */
+		hostp = gethostbyaddr((const char *) &clientaddr.sin_addr.s_addr,
+		                      sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+		if (hostp == NULL)
+			error("ERROR on gethostbyaddr");
+		hostaddrp = inet_ntoa(clientaddr.sin_addr);
+		if (hostaddrp == NULL)
+			error("ERROR on inet_ntoa\n");
+		printf("server received datagram from %s (%s)\n",
+		       hostp->h_name, hostaddrp);
+		printf("server received %ld/%d bytes: %s\n", strlen(buf), n, buf);
 
-    /* 
-     * gethostbyaddr: determine who sent the datagram
-     */
-    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-    if (hostp == NULL)
-      error("ERROR on gethostbyaddr");
-    hostaddrp = inet_ntoa(clientaddr.sin_addr);
-    if (hostaddrp == NULL)
-      error("ERROR on inet_ntoa\n");
-    printf("server received datagram from %s (%s)\n", 
-	   hostp->h_name, hostaddrp);
-    printf("server received %d/%d bytes: %s\n", strlen(buf), n, buf);
-    
-    /* 
-     * sendto: echo the input back to the client 
-     */
-    n = sendto(sockfd, buf, strlen(buf), 0, 
-	       (struct sockaddr *) &clientaddr, clientlen);
-    if (n < 0) 
-      error("ERROR in sendto");
-  }
+		/** options are:
+		 * get FILE
+		 * put FILE
+		 * delete FILE
+		 * ls
+		 * exit
+		 * unknown: repeat back to client
+		 */
+
+		trimSpace(buf);
+		command = strtok(buf, " ");
+		parameter = strtok(NULL, " ");
+
+		if (strcmp("get", command) == 0 && parameter != NULL) {
+			printf("Received command to GET file \"%s\"\n", parameter);
+			sendFile(sockfd, &clientaddr, clientlen, parameter);
+
+		} else if (strcmp("put", command) == 0 && parameter != NULL) {
+			printf("Received command to PUT file \"%s\"\n", parameter);
+			getFile(sockfd, &clientaddr, &clientlen, parameter);
+		} else if (strcmp("delete", command) == 0 && parameter != NULL) {
+			printf("Received command to DELETE file \"%s\"\n", parameter);
+			bzero(buf, BUFSIZE);
+			if (remove(parameter) == 0) {
+				sprintf(buf, "File \"%s\" deleted successfully!", parameter);
+			} else {
+				sprintf(buf, "File \"%s\" could not be deleted.", parameter);
+				// error(buf);
+			}
+			printf("Sending back \"%s\"\n", buf);
+			/*
+			 * sendto: echo the input back to the client
+			 */
+			n = sendto(sockfd, buf, strlen(buf), 0,
+			           (const struct sockaddr *) &clientaddr, clientlen);
+			if (n < 0)
+				error("ERROR in sendto");
+		} else if (strcmp("ls\n", command) == 0) {
+			printf("ls command received\n");
+			ls(buf);
+
+			printf("Sending back \"%s\"\n", buf);
+			/*
+			 * sendto: echo the input back to the client
+			 */
+			n = sendto(sockfd, buf, strlen(buf), 0,
+			           (const struct sockaddr *) &clientaddr, clientlen);
+			if (n < 0)
+				error("ERROR in sendto");
+		} else if (strcmp("exit\n", command) == 0) {
+			printf("Shutting down...\n");
+			keepRunning = 0;
+
+			printf("Sending back \"%s\"\n", buf);
+			/*
+			 * sendto: echo the input back to the client
+			 */
+			n = sendto(sockfd, buf, strlen(buf), 0,
+			           (const struct sockaddr *) &clientaddr, clientlen);
+			if (n < 0)
+				error("ERROR in sendto");
+		} else {
+			printf("wtf\n");
+		}
+
+	}
+	close(sockfd);
 }
